9cbc6397d8e0369b4d04dc5cc396b543cd5b2da1
Zahran Kabir <zahran@Zahrans-MacBook-Pro.local>
2018-03-12 16:27:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function printBadges(arr){
+  for (let i = 0; i < arr.length; i++){
+    console.log(`Welcome ${arr[i]}! You are employee #${i + 1}.`)
+  }
+  return arr
+}
+
+function maybeTrue(){
+  return Math.random() >= 0.5;
+}
+
+function tailsNeverFails(){
+  let counter = 1;
+  let statement = `You got ${counter} tails in a row`;
+  while (maybeTrue()) {
+    console.log(statement)
+    statement = `You got ${counter} tails in a row!`
+    counter++
+  }
+  return statement
+}
